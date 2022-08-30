34d151a5f6b7f637a0e494a5c421a59f3b266722
alla <zzajka13@gmail.com>
2022-08-30 22:11:37 +0300

    lesson_5 Registration

--- authapp/urls.py
+++ authapp/urls.py
@@ -7,4 +7,6 @@ app_name = AuthappConfig.name
 
 urlpatterns = [
     path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+    path("register/", views.RegisterView.as_view(), name="register"),
 ]
